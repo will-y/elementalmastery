39df105e590fa4e34375b7b75138939bc9c38001
Will Yelton <willhyelton@gmail.com>
2020-08-24 17:53:36 -0400

    Fixed boot texture and all names

--- src/main/java/eyeroh/elementalmastery/item/armor/ModArmor.java
+++ src/main/java/eyeroh/elementalmastery/item/armor/ModArmor.java
@@ -18,7 +18,7 @@ public class ModArmor {
 			() -> new ArmorItem(ArmorMaterials.OPAL, EquipmentSlotType.CHEST, new Item.Properties().group(CreativeTabs.tabGemTools)));
 	public static final RegistryObject<ArmorItem> OPAL_LEGGINGS = ARMOR.register("leggingsopal",
 			() -> new ArmorItem(ArmorMaterials.OPAL, EquipmentSlotType.LEGS, new Item.Properties().group(CreativeTabs.tabGemTools)));
-	public static final RegistryObject<ArmorItem> OPAL_BOOTS = ARMOR.register("bootstopal",
+	public static final RegistryObject<ArmorItem> OPAL_BOOTS = ARMOR.register("bootsopal",
 			() -> new ArmorItem(ArmorMaterials.OPAL, EquipmentSlotType.FEET, new Item.Properties().group(CreativeTabs.tabGemTools)));
 
 	public static final RegistryObject<ArmorItem> TOPAZ_HELMET = ARMOR.register("helmettopaz",
@@ -27,7 +27,7 @@ public class ModArmor {
 			() -> new ArmorItem(ArmorMaterials.TOPAZ, EquipmentSlotType.CHEST, new Item.Properties().group(CreativeTabs.tabGemTools)));
 	public static final RegistryObject<ArmorItem> TOPAZ_LEGGINGS = ARMOR.register("leggingstopaz",
 			() -> new ArmorItem(ArmorMaterials.TOPAZ, EquipmentSlotType.LEGS, new Item.Properties().group(CreativeTabs.tabGemTools)));
-	public static final RegistryObject<ArmorItem> TOPAZ_BOOTS = ARMOR.register("bootsttopaz",
+	public static final RegistryObject<ArmorItem> TOPAZ_BOOTS = ARMOR.register("bootstopaz",
 			() -> new ArmorItem(ArmorMaterials.TOPAZ, EquipmentSlotType.FEET, new Item.Properties().group(CreativeTabs.tabGemTools)));
 
 	public static final RegistryObject<ArmorItem> RUBY_HELMET = ARMOR.register("helmetruby",
@@ -36,7 +36,7 @@ public class ModArmor {
 			() -> new ArmorItem(ArmorMaterials.RUBY, EquipmentSlotType.CHEST, new Item.Properties().group(CreativeTabs.tabGemTools)));
 	public static final RegistryObject<ArmorItem> RUBY_LEGGINGS = ARMOR.register("leggingsruby",
 			() -> new ArmorItem(ArmorMaterials.RUBY, EquipmentSlotType.LEGS, new Item.Properties().group(CreativeTabs.tabGemTools)));
-	public static final RegistryObject<ArmorItem> RUBY_BOOTS = ARMOR.register("bootstruby",
+	public static final RegistryObject<ArmorItem> RUBY_BOOTS = ARMOR.register("bootsruby",
 			() -> new ArmorItem(ArmorMaterials.RUBY, EquipmentSlotType.FEET, new Item.Properties().group(CreativeTabs.tabGemTools)));
 
 	public static final RegistryObject<ArmorItem> SAPPHIRE_HELMET = ARMOR.register("helmetsapphire",
@@ -45,7 +45,7 @@ public class ModArmor {
 			() -> new ArmorItem(ArmorMaterials.SAPPHIRE, EquipmentSlotType.CHEST, new Item.Properties().group(CreativeTabs.tabGemTools)));
 	public static final RegistryObject<ArmorItem> SAPPHIRE_LEGGINGS = ARMOR.register("leggingssapphire",
 			() -> new ArmorItem(ArmorMaterials.SAPPHIRE, EquipmentSlotType.LEGS, new Item.Properties().group(CreativeTabs.tabGemTools)));
-	public static final RegistryObject<ArmorItem> SAPPHIRE_BOOTS = ARMOR.register("bootstsapphire",
+	public static final RegistryObject<ArmorItem> SAPPHIRE_BOOTS = ARMOR.register("bootssapphire",
 			() -> new ArmorItem(ArmorMaterials.SAPPHIRE, EquipmentSlotType.FEET, new Item.Properties().group(CreativeTabs.tabGemTools)));
 
 	public static final RegistryObject<SpecialArmor> SPEED_HELMET = ARMOR.register("helmetspeed",
@@ -54,7 +54,7 @@ public class ModArmor {
 			() -> new SpecialArmor(ArmorMaterials.SPEED, EquipmentSlotType.CHEST));
 	public static final RegistryObject<SpecialArmor> SPEED_LEGGINGS = ARMOR.register("leggingsspeed",
 			() -> new SpecialArmor(ArmorMaterials.SPEED, EquipmentSlotType.LEGS));
-	public static final RegistryObject<SpecialArmor> SPEED_BOOTS = ARMOR.register("bootstspeed",
+	public static final RegistryObject<SpecialArmor> SPEED_BOOTS = ARMOR.register("bootsspeed",
 			() -> new SpecialArmor(ArmorMaterials.SPEED, EquipmentSlotType.FEET));
 
 	public static final RegistryObject<SpecialArmor> FIRE_HELMET = ARMOR.register("helmetfire",
@@ -63,7 +63,7 @@ public class ModArmor {
 			() -> new SpecialArmor(ArmorMaterials.FIRE, EquipmentSlotType.CHEST));
 	public static final RegistryObject<SpecialArmor> FIRE_LEGGINGS = ARMOR.register("leggingsfire",
 			() -> new SpecialArmor(ArmorMaterials.FIRE, EquipmentSlotType.LEGS));
-	public static final RegistryObject<SpecialArmor> FIRE_BOOTS = ARMOR.register("bootstfire",
+	public static final RegistryObject<SpecialArmor> FIRE_BOOTS = ARMOR.register("bootsfire",
 			() -> new SpecialArmor(ArmorMaterials.FIRE, EquipmentSlotType.FEET));
 
 	public static final RegistryObject<SpecialArmor> HEAL_HELMET = ARMOR.register("helmetheal",
@@ -72,7 +72,7 @@ public class ModArmor {
 			() -> new SpecialArmor(ArmorMaterials.HEAL, EquipmentSlotType.CHEST));
 	public static final RegistryObject<SpecialArmor> HEAL_LEGGINGS = ARMOR.register("leggingsheal",
 			() -> new SpecialArmor(ArmorMaterials.HEAL, EquipmentSlotType.LEGS));
-	public static final RegistryObject<SpecialArmor> HEAL_BOOTS = ARMOR.register("bootstheal",
+	public static final RegistryObject<SpecialArmor> HEAL_BOOTS = ARMOR.register("bootsheal",
 			() -> new SpecialArmor(ArmorMaterials.HEAL, EquipmentSlotType.FEET));
 
 	public static final RegistryObject<SpecialArmor> STRENGTH_HELMET = ARMOR.register("helmetstrength",
@@ -81,7 +81,7 @@ public class ModArmor {
 			() -> new SpecialArmor(ArmorMaterials.STRENGTH, EquipmentSlotType.CHEST));
 	public static final RegistryObject<SpecialArmor> STRENGTH_LEGGINGS = ARMOR.register("leggingsstrength",
 			() -> new SpecialArmor(ArmorMaterials.STRENGTH, EquipmentSlotType.LEGS));
-	public static final RegistryObject<SpecialArmor> STRENGTH_BOOTS = ARMOR.register("bootststrength",
+	public static final RegistryObject<SpecialArmor> STRENGTH_BOOTS = ARMOR.register("bootsstrength",
 			() -> new SpecialArmor(ArmorMaterials.STRENGTH, EquipmentSlotType.FEET));
 
 }
