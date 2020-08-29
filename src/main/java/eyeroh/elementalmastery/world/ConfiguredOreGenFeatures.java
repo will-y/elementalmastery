4c659f6a438866c56dbb36201ea573cc91079179
Will Yelton <willhyelton@gmail.com>
2020-08-29 14:22:49 -0400

    Reduced the spawn rate of ores and made them only drop one gem each (without fortune)

--- src/main/java/eyeroh/elementalmastery/world/ConfiguredOreGenFeatures.java
+++ src/main/java/eyeroh/elementalmastery/world/ConfiguredOreGenFeatures.java
@@ -10,10 +10,10 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class ConfiguredOreGenFeatures {
-    public static ConfiguredFeature<?, ?> ORE_OPAL = newConfiguredFeature("ore_opal", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.OPAL_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(3));
-    public static ConfiguredFeature<?, ?> ORE_TOPAZ = newConfiguredFeature("ore_topaz", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.TOPAZ_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(3));
-    public static ConfiguredFeature<?, ?> ORE_RUBY = newConfiguredFeature("ore_ruby", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.RUBY_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(3));
-    public static ConfiguredFeature<?, ?> ORE_SAPPHIRE = newConfiguredFeature("ore_sapphire", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.SAPPHIRE_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(3));
+    public static ConfiguredFeature<?, ?> ORE_OPAL = newConfiguredFeature("ore_opal", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.OPAL_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(2));
+    public static ConfiguredFeature<?, ?> ORE_TOPAZ = newConfiguredFeature("ore_topaz", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.TOPAZ_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(2));
+    public static ConfiguredFeature<?, ?> ORE_RUBY = newConfiguredFeature("ore_ruby", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.RUBY_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(2));
+    public static ConfiguredFeature<?, ?> ORE_SAPPHIRE = newConfiguredFeature("ore_sapphire", Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.field_241882_a, ModBlocks.SAPPHIRE_ORE.get().getDefaultState(), 8)).func_242733_d(25).func_242728_a().func_242731_b(2));
 
     public static ConfiguredFeature<?, ?> newConfiguredFeature(String registryName, ConfiguredFeature<?, ?> configuredFeature) {
         Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(ElementalMastery.MODID, registryName), configuredFeature);
