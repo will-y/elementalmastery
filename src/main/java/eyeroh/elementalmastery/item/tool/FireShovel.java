a6efe9c13610472c65266e7691d52644dae5e5d0
Will Yelton <willhyelton@gmail.com>
2020-09-04 00:05:58 -0400

    Fixed flame shovel stoping working after a few blocks (probably)

--- src/main/java/eyeroh/elementalmastery/item/tool/FireShovel.java
+++ src/main/java/eyeroh/elementalmastery/item/tool/FireShovel.java
@@ -5,10 +5,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShovelItem;
+import net.minecraft.item.crafting.AbstractCookingRecipe;
 import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.loot.LootContext;
 import net.minecraft.loot.LootParameters;
 import net.minecraft.util.math.BlockPos;
@@ -16,6 +19,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.ToolType;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class FireShovel extends ShovelItem {
@@ -30,11 +34,12 @@ public class FireShovel extends ShovelItem {
             if(state.getHarvestTool() == ToolType.SHOVEL || state.getHarvestTool() == null) {
                 List<ItemStack> drops = state.getDrops(new LootContext.Builder((ServerWorld) world).withParameter(LootParameters.TOOL, stack).withParameter(LootParameters.field_237457_g_, entityLiving.getLookVec()));
                 for (ItemStack itemStack : drops) {
-                    world.getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(itemStack), world).ifPresent((furnaceRecipe) -> {
-                        ItemStack smelted = furnaceRecipe.getRecipeOutput();
+                    Inventory inv = new Inventory(itemStack);
+                    if (world.getRecipeManager().getRecipe(IRecipeType.SMELTING, inv, world).isPresent()) {
+                        ItemStack smelted = world.getRecipeManager().getRecipe(IRecipeType.SMELTING, inv, world).get().getRecipeOutput();
                         world.destroyBlock(pos, false);
                         Block.spawnAsEntity(world, pos, smelted);
-                    });
+                    }
                 }
             }
         }
